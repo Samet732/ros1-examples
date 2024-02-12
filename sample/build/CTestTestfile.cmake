0d3726c2f6e15fc691f52ffb0c554baa721b8725
Samet732 <Samethub732@gmail.com>
2024-02-12 13:37:55 +0300

    deleted build files

--- sample/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/samet/workspace/sample/src
-# Build directory: /home/samet/workspace/sample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
